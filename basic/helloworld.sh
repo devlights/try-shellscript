9f69bd054819ea314c5c2774114f0a5edab3d41e
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-09-09 11:43:21 +0900

    Update basic/helloworld.sh

--- basic/helloworld.sh
+++ basic/helloworld.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-echo 'Hello World!'
+# HelloWorld -- Hello World サンプルです.
+_exec() {
+    # 最もシンプルな形
+    echo 'Hello World!'
+
+    # printf を利用することもできる
+    h='Hello'
+    w='World'
+    printf "%s %s!\n" ${h} ${w}
+}
+
+_exec
