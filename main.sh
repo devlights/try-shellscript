97fe64e2b02eb9e50501acea2622765badd0351d
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-12-08 04:06:11 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -18,6 +18,10 @@ _make_mappings() {
 #   - https://qiita.com/kaw/items/034bc4221c4526fe8866
 #   - https://eng-entrance.com/linux-shellscript-keyboard
 #   - https://eng-entrance.com/linux-shellscript-function
+#   - https://qiita.com/b4b4r07/items/e56a8e3471fb45df2f59
+#   - https://stackoverflow.com/questions/229551/how-to-check-if-a-string-contains-a-substring-in-bash
+#   - https://qiita.com/b4b4r07/items/cb02bbfd6df58df06ea8
+#   - https://www.atmarkit.co.jp/ait/articles/1905/30/news017.html
 _main() {
     onetime=false
     while getopts o option
@@ -52,9 +56,40 @@ _main() {
                 continue;;
         esac
 
-        script_path="${mappings[${name}]}"
+        # Find candidates
+        declare -a candidates=()
+        count=0
+        for key in "${!mappings[@]}"
+        do 
+            if [[ "$key" == *"$name"* ]]; then
+                candidates=("${candidates[@]}" "$key")
+                let count++
+            fi
+        done
+
+        if [ "$count" -eq 0 ]; then
+            echo -e '\nNo candidates were found...Please try again.\n'
+            continue;
+        fi
+
+        if [ "$count" -gt 1 ]; then
+            echo -e '\nMultiple candidates were found...'
+            echo '-----------------------------------'
+
+            for ((i=0; i < ${#candidates[@]}; i++)) {
+                echo "${candidates[i]}"
+            }
+
+            echo -e '-----------------------------------\n'
+            continue;
+        fi
+
+        # There is only one candidate, execute it.
+        target="${candidates[0]}"
+        script_path="${mappings[${target}]}"
 
         echo "[INPUT ] ${name}"
+        echo "[TARGET] ${target}"
         echo "[SCRIPT] ${script_path}"
 
         echo "===== START [${script_path}] ====="
